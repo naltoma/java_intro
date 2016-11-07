36c8c90832054ae87e702f2de5c0a3c5dd76c7ef
Naruaki TOMA <tnal@ie.u-ryukyu.ac.jp>
2016-11-07 15:59:03 +0900

    Add a result of Ex1_1

---
+++ Ex1/Ex1_1.java
@@ -0,0 +1,23 @@
+/**
+ * Ex1_1 on ex1.md
+ */
+public class Ex1_1 {
+    private static String reversed(String source){
+        int i, j;
+        char[] temp = new char[source.length()];
+        for(i=source.length()-1, j=0; i>=0; i--, j++){
+            temp[j] = source.charAt(i);
+        }
+        //String target = new String(temp);
+        //return temp.toString();
+        //return target;
+        return new String(temp);
+    }
+
+    public static void main(String[] args){
+        String source = "stressed";
+        System.out.println("source = " + source);
+        String target = reversed(source);
+        System.out.println("target = " + target);
+    }
+}
